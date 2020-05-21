afe7da5676f04fef7aaa6a1e07aac87bbf4194aa
Jan Kroeze <jcwkroeze@protonmail.com>
2020-05-21 23:38:32 +0200

    Create devices and queues

--- src/main.zig
+++ src/main.zig
@@ -4,6 +4,26 @@ const vk = @import("vk");
 
 const allocator = std.heap.c_allocator;
 
+pub fn createDevice(gpu: vk.PhysicalDevice) anyerror!vk.Device {
+    const queue = [_]vk.DeviceQueueCreateInfo{ try createQueue() };
+    const createInfo = vk.DeviceCreateInfo{
+        .queueCreateInfoCount = 1,
+        .pQueueCreateInfos = &queue,
+        .enabledExtensionCount = 0,
+        .ppEnabledExtensionNames = undefined
+    };
+    const result = vk.CreateDevice(gpu, createInfo, null);
+    return result;
+}
+
+pub fn createGPU(instance: vk.Instance) anyerror!vk.PhysicalDevice {
+    const deviceCount = try vk.EnumeratePhysicalDevicesCount(instance);
+    const physicalDevices = try allocator.alloc(vk.PhysicalDevice, deviceCount);
+    defer allocator.free(physicalDevices);
+    var result = try vk.EnumeratePhysicalDevices(instance, physicalDevices);
+    return result.physicalDevices[0];
+}
+
 pub fn createInstance() anyerror!vk.Instance {
     var appInfo = vk.ApplicationInfo{
         .applicationVersion = 0,
@@ -17,12 +37,22 @@ pub fn createInstance() anyerror!vk.Instance {
     return instance;
 }
 
+pub fn createQueue() anyerror!vk.DeviceQueueCreateInfo {
+    const priority = [_]f32{1};
+    const result = vk.DeviceQueueCreateInfo{
+        .queueCount = 1,
+        .queueFamilyIndex = 0,
+        .pQueuePriorities = &priority
+    };
+    return result;
+}
+
 pub fn printVulkanVersion() anyerror!void {
     var version = try vk.EnumerateInstanceVersion();
     var major = version >> 22;
     var minor = (version >> 12) & 0x3ff;
     var patch = version & 0xfff;
-    std.debug.warn("vulkan {}.{}.{}", .{major, minor, patch});
+    std.debug.warn("vulkan {}.{}.{}\n", .{major, minor, patch});
 }
 
 pub fn main() anyerror!void {
@@ -33,13 +63,12 @@ pub fn main() anyerror!void {
         640, 480,
         0
     );
+
     try printVulkanVersion();
     var instance = try createInstance();
-
-    const deviceCount = try vk.EnumeratePhysicalDevicesCount(instance);
-    const physicalDevices = try allocator.alloc(vk.PhysicalDevice, deviceCount);
-    defer allocator.free(physicalDevices);
-    var vkResult = try vk.EnumeratePhysicalDevices(instance, physicalDevices);
+    var gpu = try createGPU(instance);
+    var device = try createDevice(gpu);
+    var queue = vk.GetDeviceQueue(device, 0, 0);
 
     var done = false;
     while (!done) {
