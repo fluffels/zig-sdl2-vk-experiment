9814fbadb8d7d9dafb6dcea7f184c13bcf60a863
Jan Kroeze <jcwkroeze@protonmail.com>
2020-05-22 10:46:32 +0200

    fetch required vulkan extensions for SDL

--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,7 @@
-const sdl = @cImport(@cInclude("SDL.h"));
-const sdlVK = @cImport(@cInclude("SDL_Vulkan.h"));
+const sdl = @cImport({
+    @cInclude("SDL.h");
+    @cInclude("SDL_Vulkan.h");
+});
 const std = @import("std");
 const vk = @import("vk");
 
@@ -29,7 +31,7 @@ pub fn createInstance() anyerror!vk.Instance {
     var appInfo = vk.ApplicationInfo{
         .applicationVersion = 0,
         .engineVersion = 0,
-        .apiVersion = 0
+        .apiVersion = 0,
     };
     var createInfo = vk.InstanceCreateInfo{
         .pApplicationInfo = &appInfo
@@ -56,26 +58,60 @@ pub fn printVulkanVersion() anyerror!void {
     std.debug.warn("vulkan {}.{}.{}\n", .{major, minor, patch});
 }
 
+pub fn getRequiredExtensions(window: *sdl.SDL_Window) ![][*:0]u8 {
+    var count: c_uint = 0;
+    var success = sdl.SDL_Vulkan_GetInstanceExtensions(
+        window, &count, null
+    );
+    if (@enumToInt(success) != sdl.SDL_TRUE) {
+        std.debug.warn("could not fetch extension count", .{});
+        // return;
+    } else {
+        std.debug.warn("requires {} extensions\n", .{ count });
+    }
+    var names = try allocator.alloc([*:0]u8, count);
+    for (names) |*name| {
+        name.* = @ptrCast([*:0]u8, try allocator.alloc(u8, 255));
+    }
+    success = sdl.SDL_Vulkan_GetInstanceExtensions(
+        window,
+        &count,
+        @ptrCast([*c][*c]u8, @alignCast(8, names))
+    );
+    if (@enumToInt(success) != sdl.SDL_TRUE) {
+        std.debug.warn("could not fetch extensions", .{});
+        // return;
+    } else {
+        std.debug.warn("fetched {} extensions\n", .{ names.len });
+    }
+    for (names) |name| {
+        std.debug.warn("required extension: {}\n", . { name });
+    }
+    return names;
+}
+
 pub fn main() anyerror!void {
     var code = sdl.SDL_Init(sdl.SDL_INIT_VIDEO);
     var window = sdl.SDL_CreateWindow(
         "skybox",
         0, 0,
         640, 480,
-        0
-    );
+        sdl.SDL_WINDOW_VULKAN
+    ) orelse return;
 
     try printVulkanVersion();
     var instance = try createInstance();
 
-    var surface: vk.SurfaceKHR = undefined;
-    code = sdlVK.SDL_Vulkan_CreateSurface(
+    var extensions = getRequiredExtensions(window);
+
+    var surface: sdl.VkSurfaceKHR = undefined;
+    var success = sdl.SDL_Vulkan_CreateSurface(
         window,
-        instance,
+        @ptrCast(sdl.VkInstance, &instance),
         &surface
     );
-    if (!code) {
-        std.debug.warn("could not create surface");
+    if (@enumToInt(success) != sdl.SDL_TRUE) {
+        std.debug.warn("could not create surface", .{});
         return;
     }
 
